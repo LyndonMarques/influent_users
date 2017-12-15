5bd323901c0a47babadc51bc0d16917410a0d245
DESKTOP-GBOT7HK\Lyndon <lyndon.omarques@gmail.com>
2017-12-15 15:07:13 -0200

    Upload assets

---
+++ application/models/Loginmodel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Lyndon
+ * Date: 15/12/2017
+ * Time: 14:01
+ */
+class Loginmodel extends CI_Model {
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function get_login(){
+		
+		return "model logi";
+	}
+	
+}
