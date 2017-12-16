10f70fcab231caaab20cf72616d09c112895236e
DESKTOP-GBOT7HK\Lyndon <lyndon.omarques@gmail.com>
2017-12-15 23:42:19 -0200

    FInalizado criação do array com todos os dados de usuarios e marcas relacionados ao arquivo interactions.json.

--- application/controllers/Interactions.php
+++ application/controllers/Interactions.php
@@ -6,11 +6,16 @@ class Interactions extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper('url');
+		$this->load->model("interactionmodel");
 	}
 	
 	public function index()
 	{
+		$selectIteractions = $this->interactionmodel->get_interactions();
+		$data["interacoes"] = $selectIteractions;
 		
-		$this->load->view('interactions');
+		echo "<pre>";
+		print_r($data);die;
+		$this->load->view('interactions', $data);
 	}
 }
--- application/models/Interactionmodel.php
+++ application/models/Interactionmodel.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: Lyndon
- * Date: 15/12/2017
- * Time: 22:34
- */
 class Interactionmodel extends CI_Model {
 	
 	public function __construct() {
@@ -13,8 +7,47 @@ class Interactionmodel extends CI_Model {
 	}
 	
 	public function get_interactions(){
+		
+		$urlfileinteractions  = "application/models/data/interactions.json";
+		$urlfilebrands        = "application/models/data/brands.json";
+		$urlfileusers         = "application/models/data/users.json";
+		
+		$str_data_iteractions = file_get_contents($urlfileinteractions);
+		$str_data_brands      = file_get_contents($urlfilebrands);
+		$str_data_users       = file_get_contents($urlfileusers);
+		
+		$data_iteractions     = json_decode($str_data_iteractions, true);
+		$data_brands          = json_decode($str_data_brands, true);
+		$data_users           = json_decode($str_data_users, true);
+		
+		
+		foreach($data_iteractions as $key => $row){
+			//---------------------------Brands---------------------------//
+			//Id da marca no arquivo interactions.json
+			$id_brand             = $data_iteractions[$key]['brand'];
+			//Pega id da marca relacionado ao id brand no arquivo interations.json
+			$getBrandsKey         = $this->searchForId($id_brand, $data_brands);
+			//Inseri a key marca com todos os dados da marca no array interactions
+			$data_iteractions[$key]['marcas'] = $data_brands[$getBrandsKey];
+			//---------------------------Users---------------------------//
+			//Id do usuario no arquivo interactions.json
+			$id_users             = $data_iteractions[$key]['user'];
+			//Pega id do usuário relacionado ao id users no arquivo interations.json
+			$getUsersKey         = $this->searchForId($id_users, $data_users);
+			//Inseri a key usuarios com todos os dados do usuário no array interactions
+			$data_iteractions[$key]['usuarios'] = $data_users[$getUsersKey];
+		}
+		
+		return $data_iteractions;
+	}
 	
-	
+	function searchForId($id, $array) {
+		foreach ($array as $key => $val) {
+			if ($val['id'] === $id) {
+				return $key;
+			}
+		}
+		return null;
 	}
 	
 }
