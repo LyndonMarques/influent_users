150cbba275796463965e8f073903d5969d06e1b4
DESKTOP-GBOT7HK\Lyndon <lyndon.omarques@gmail.com>
2017-12-15 22:37:39 -0200

    Criado class Interactionmodel.

---
+++ application/models/Interactionmodel.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Lyndon
+ * Date: 15/12/2017
+ * Time: 22:34
+ */
+class Interactionmodel extends CI_Model {
+	
+	public function __construct() {
+		parent::__construct();
+	}
+	
+	public function get_interactions(){
+	
+	
+	}
+	
+}
